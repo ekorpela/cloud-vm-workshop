db1312cf4ea3ac044778ea66c22d369e1c71aa19
Stefano Nicotri <stefano.nicotri@gmail.com>
2016-05-24 11:38:21 +0300

    updated stefano nicotri's files

--- materials/stefano_nicotri/RStudio_Docker_user_data.sh
+++ materials/stefano_nicotri/RStudio_Docker_user_data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 sudo sed -i 's/^.*ssh/ssh/' /root/.ssh/authorized_keys
-sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get -y autoremove && sudo apt-get -y install docker.io
+sudo apt-get update
+#sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get -y autoremove
+sudo apt-get -y install docker.io
 sudo docker run -d -p 80:8787 rocker/rstudio
